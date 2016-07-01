6b6ad72c99aaeb3af35e8f9f8b88728231075a27
TAN <atathunder@gmail.com>
2016-07-02 03:29:33 +0800

    for wins

--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import os
 from app import create_app, db
-from app.models import User,Grade,Task,Learn
+from app.models import User,Grade,Task,Learn,Course,Teach
 from flask.ext.script import Manager, Shell
 from flask.ext.migrate import Migrate, MigrateCommand
 
